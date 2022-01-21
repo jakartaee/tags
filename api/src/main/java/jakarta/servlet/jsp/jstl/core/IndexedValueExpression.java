eaed10aa006bb919a4d4670927a03fc57b4d5334
Paul Nicolucci <pnicolucci@gmail.com>
2022-01-20 23:50:01 -0500

    Issue #166: Update copyright headers

--- api/src/main/java/jakarta/servlet/jsp/jstl/core/IndexedValueExpression.java
+++ api/src/main/java/jakarta/servlet/jsp/jstl/core/IndexedValueExpression.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 1997-2020 Oracle and/or its affiliates. All rights reserved.
  * Copyright 2004 The Apache Software Foundation
+ * Copyright (c) 2022-2022 Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
